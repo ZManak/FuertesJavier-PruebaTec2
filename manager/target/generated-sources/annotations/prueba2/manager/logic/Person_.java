29cb741d8d6eeb32651403357edef28e9bc3726c
ZManak <javiefe.dev@gmail.com>
2024-01-23 14:59:11 +0100

    todo relacion front-back

---
+++ manager/target/generated-sources/annotations/prueba2/manager/logic/Person_.java
@@ -0,0 +1,14 @@
+package prueba2.manager.logic;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-01-23T14:53:15")
+@StaticMetamodel(Person.class)
+public class Person_ { 
+
+    public static volatile SingularAttribute<Person, String> name;
+    public static volatile SingularAttribute<Person, Integer> id;
+
+}
---
+++ manager/target/generated-sources/annotations/prueba2/manager/logic/Turn_.java
@@ -0,0 +1,19 @@
+package prueba2.manager.logic;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-01-23T14:53:15")
+@StaticMetamodel(Turn.class)
+public class Turn_ { 
+
+    public static volatile SingularAttribute<Turn, Long> number;
+    public static volatile SingularAttribute<Turn, Date> onDate;
+    public static volatile SingularAttribute<Turn, String> description;
+    public static volatile SingularAttribute<Turn, Integer> personId;
+    public static volatile SingularAttribute<Turn, Integer> id;
+    public static volatile SingularAttribute<Turn, Boolean> done;
+
+}
